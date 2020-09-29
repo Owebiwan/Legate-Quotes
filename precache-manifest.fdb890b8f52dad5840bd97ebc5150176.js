5a568f8d4291a5baddf17af85bbab5c7a760de34
Godwin Obamina <38545029+Owebiwan@users.noreply.github.com>
2020-09-29 20:01:30 +0100

    Updates

---
+++ precache-manifest.fdb890b8f52dad5840bd97ebc5150176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35f490bd578fc7f9a63178660fa078f",
+    "url": "/Legate-Quotes/index.html"
+  },
+  {
+    "revision": "3e6cc10d636a126fe08e",
+    "url": "/Legate-Quotes/static/css/main.bcaf1831.chunk.css"
+  },
+  {
+    "revision": "371065bbb16b8c9a2b52",
+    "url": "/Legate-Quotes/static/js/2.31a3e4b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Legate-Quotes/static/js/2.31a3e4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6cc10d636a126fe08e",
+    "url": "/Legate-Quotes/static/js/main.64056b99.chunk.js"
+  },
+  {
+    "revision": "90a8e4a04441f2b25f28",
+    "url": "/Legate-Quotes/static/js/runtime-main.e74bc408.js"
+  }
+]);
